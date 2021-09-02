da0a43c9e3180278c4eee669e66be2598f27329e
Christian Mantilla <mantilla.chris@gmail.com>
2021-09-02 17:44:06 +0100

    Initial commit. Scraping functions defined in jupyter notebook.

---
+++ cfc/scraper.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup as bs
+import json
+import re
+import requests
+import os
+from pathlib import Path
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+
+
