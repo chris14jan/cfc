badb55d9711b2a3fc3bc510641d5febd7170acf1
Christian Mantilla <mantilla.chris@gmail.com>
2021-09-12 00:27:01 +0100

    Removed duplicate test

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -8,6 +8,5 @@ class TestScraper(unittest.TestCase):
         self.assertIsInstance(links, list, "The `parse_links` method should return a `list`")
         self.assertIsInstance(links[0], str, "The `parse_links` method should return a `str` with a url or file path name for each element in the list")
         self.assertEqual(links[0], 'https://www.cfcunderwriting.com/en-gb/')
-        self.assertEqual(links[5], 'https://www.cfcunderwriting.com/en-gb/')
         self.assertEqual(links[-1], "background-image: url('https://cfccorpsite.azureedge.net/cache/9/b/0/a/d/b/9b0adbf131c63661d836a600698a5ac48a1e90f0.png');")
         pass
